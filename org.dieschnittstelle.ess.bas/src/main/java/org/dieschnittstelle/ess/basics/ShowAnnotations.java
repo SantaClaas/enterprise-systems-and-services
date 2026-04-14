13bfb4f4d92654d834a21751ab0bea0d23576f92
Claas <claas@claas.dev>
2026-04-14 22:52:40 +0200

    Finish BAS2 assignment

--- org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
+++ org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
@@ -4,53 +4,88 @@ package org.dieschnittstelle.ess.basics;
 import org.dieschnittstelle.ess.basics.annotations.AnnotatedStockItemBuilder;
 import org.dieschnittstelle.ess.basics.annotations.StockItemProxyImpl;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.dieschnittstelle.ess.basics.reflection.ReflectedStockItemBuilder.getAccessorNameForField;
 import static org.dieschnittstelle.ess.utils.Utils.*;
 
 public class ShowAnnotations {
 
-	public static void main(String[] args) {
-		// we initialise the collection
-		StockItemCollection collection = new StockItemCollection(
-				"stockitems_annotations.xml", new AnnotatedStockItemBuilder());
-		// we load the contents into the collection
-		collection.load();
+    public static void main(String[] args) {
+        // we initialise the collection
+        StockItemCollection collection = new StockItemCollection(
+                "stockitems_annotations.xml", new AnnotatedStockItemBuilder());
+        // we load the contents into the collection
+        collection.load();
 
-		for (IStockItem consumable : collection.getStockItems()) {
-			showAttributes(((StockItemProxyImpl)consumable).getProxiedObject());
-		}
+        for (IStockItem consumable : collection.getStockItems()) {
+            showAttributes(((StockItemProxyImpl) consumable).getProxiedObject());
+        }
 
-		// we initialise a consumer
-		Consumer consumer = new Consumer();
-		// ... and let them consume
-		consumer.doShopping(collection.getStockItems());
-	}
+        // we initialise a consumer
+        Consumer consumer = new Consumer();
+        // ... and let them consume
+        consumer.doShopping(collection.getStockItems());
+    }
 
-	/*
-	 * TODO BAS2
-	 */
-	private static void showAttributes(Object instance) {
-		show("class is: " + instance.getClass());
 
-		try {
+    private static void runBas2(Object instance) {
+        var class_ = instance.getClass();
 
-			// TODO BAS2: create a string representation of instance by iterating
-			//  over the object's attributes / fields as provided by its class
-			//  and reading out the attribute values. The string representation
-			//  will then be built from the field names and field values.
-			//  Note that only read-access to fields via getters or direct access
-			//  is required here.
+        var fields = class_.getDeclaredFields();
+        // Easiest tuple like value I could find is Map.Entry
+        var values = new ArrayList<Map.Entry<String, String>>();
+        for (var field : fields) {
+            var fieldName = field.getName();
+            var accessorName = getAccessorNameForField("get", fieldName);
+            Method accessor;
+//			TODO handle accessor not existing by either skipping field or accessing it directly
+            try {
+                accessor = class_.getMethod(accessorName);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
 
-			// TODO BAS3: if the new @DisplayAs annotation is present on a field,
-			//  the string representation will not use the field's name, but the name
-			//  specified in the the annotation. Regardless of @DisplayAs being present
-			//  or not, the field's value will be included in the string representation.
+            Object fieldValue;
+            try {
+                fieldValue = accessor.invoke(instance);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
 
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException("showAnnotations(): exception occurred: " + e,e);
-		}
+            values.add(new AbstractMap.SimpleEntry<>(fieldName, fieldValue.toString()));
+        }
 
-	}
+
+        var className = class_.getSimpleName();
+
+        var valuesList = values.stream().map(pair -> pair.getKey() + ":" + pair.getValue()).collect(Collectors.joining(", "));
+        System.out.printf("{%s %s}%n", className, valuesList);
+    }
+
+    /*
+     * TODO BAS2
+     */
+    private static void showAttributes(Object instance) {
+        show("class is: " + instance.getClass());
+
+
+        try {
+            runBas2(instance);
+
+            // TODO BAS3: if the new @DisplayAs annotation is present on a field,
+            //  the string representation will not use the field's name, but the name
+            //  specified in the the annotation. Regardless of @DisplayAs being present
+            //  or not, the field's value will be included in the string representation.
+
+        } catch (Exception error) {
+            error.printStackTrace();
+            throw new RuntimeException("showAnnotations(): exception occurred: " + error, error);
+        }
+
+    }
 
 }
