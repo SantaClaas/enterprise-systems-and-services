4ccdfada5207ba889ae0675817bf620712129761
Claas <claas@claas.dev>
2026-04-21 11:44:53 +0200

    Add code for exercise

--- org.dieschnittstelle.ess.ser/src/main/java/org/dieschnittstelle/ess/ser/TouchpointGUIServlet.java
+++ org.dieschnittstelle.ess.ser/src/main/java/org/dieschnittstelle/ess/ser/TouchpointGUIServlet.java
@@ -13,110 +13,113 @@ import static org.dieschnittstelle.ess.utils.Utils.show;
 
 public class TouchpointGUIServlet extends HttpServlet {
 
-	protected static Logger logger = org.apache.logging.log4j.LogManager
-			.getLogger(TouchpointGUIServlet.class);
+    protected static Logger logger = org.apache.logging.log4j.LogManager
+            .getLogger(TouchpointGUIServlet.class);
 
-	private boolean demoPostNonIdempotence = false;
-	private boolean demoGetIdempotence = false;
+    private boolean demoPostNonIdempotence = false;
+    private boolean demoGetIdempotence = false;
 
-	public TouchpointGUIServlet() {
-		show("TouchpointGUIServlet: constructor invoked\n");
-	}
+    public TouchpointGUIServlet() {
+        show("TouchpointGUIServlet: constructor invoked\n");
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest request,
-						 HttpServletResponse response) {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) {
 
-		show("TouchpointGUIServlet: doGet() invoked\n");
+        show("TouchpointGUIServlet: doGet() invoked\n");
 
-		// this can be used to demonstrate browser behaviour
-		// for refreshes of POST vs. GET requests for the create action.
-		// change method attribute in jsp for trying it out.
-		if (request.getPathInfo().startsWith("/create"))  {
-			doPost(request,response);
-		}
-		else {
-			if (demoGetIdempotence) {
-				request.setAttribute("demoGetIdempotence",true);
-			}
-			displayView(request, response);
-		}
-	}
 
-	@Override
-	protected void doPost(HttpServletRequest request,
-						  HttpServletResponse response) {
+        show("Path INFO %s", request.getPathInfo());
+        var pathInfo = request.getPathInfo();
+        // this can be used to demonstrate browser behaviour
+        // for refreshes of POST vs. GET requests for the create action.
+        // change method attribute in jsp for trying it out.
+        if (pathInfo != null && pathInfo.startsWith("/create")) {
+            doPost(request, response);
+            return;
+        }
 
-		show("TouchpointGUIServlet: doPost() invoked\n");
+        if (demoGetIdempotence) {
+            request.setAttribute("demoGetIdempotence", true);
+        }
 
-		// obtain the executor
-		TouchpointCRUDExecutor exec = (TouchpointCRUDExecutor) getServletContext()
-				.getAttribute("touchpointCRUD");
+        displayView(request, response);
+    }
 
-		// obtain the pathInfo value
-		String pathInfo = request.getPathInfo();
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) {
 
-		// check whether we need to create or delete a touchpoint
-		if (pathInfo.startsWith("/delete")) {
-			// if we have a delete request, the id to be deleted will be the
-			// last segment of the path
-			int id = Integer.parseInt(pathInfo.substring(pathInfo
-					.lastIndexOf("/") + 1));
-			exec.deleteTouchpoint(id);
-		} else if (request.getPathInfo().startsWith("/create")) {
-			String name = request.getParameter("name");
+        show("TouchpointGUIServlet: doPost() invoked\n");
 
-			// if we have a create request, we read out the parameters
-			String street = request.getParameter("street");
-			String houseNr = request.getParameter("houseNr");
-			String city = request.getParameter("city");
-			String zipCode = request.getParameter("zipCode");
+        // obtain the executor
+        TouchpointCRUDExecutor exec = (TouchpointCRUDExecutor) getServletContext()
+                .getAttribute("touchpointCRUD");
 
-			// create a new address
-			Address addr = new Address(street, houseNr, zipCode, city);
-			StationaryTouchpoint tp = new StationaryTouchpoint(-1, name, addr);
-			exec.createTouchpoint(tp);
-		}
+        // obtain the pathInfo value
+        String pathInfo = request.getPathInfo();
 
-		if (!demoPostNonIdempotence && !demoGetIdempotence) {
-			request.setAttribute("redirectToRoot",true);
-		}
-		else if (demoGetIdempotence) {
-			request.setAttribute("demoGetIdempotence",true);
-		}
-		displayView(request, response);
-	}
+        // check whether we need to create or delete a touchpoint
+        if (pathInfo.startsWith("/delete")) {
+            // if we have a delete request, the id to be deleted will be the
+            // last segment of the path
+            int id = Integer.parseInt(pathInfo.substring(pathInfo
+                    .lastIndexOf("/") + 1));
+            exec.deleteTouchpoint(id);
+        } else if (request.getPathInfo().startsWith("/create")) {
+            String name = request.getParameter("name");
 
-	/**
-	 * display the view (we always use the same jsp for all requests)
-	 */
-	/*
-	 * now pass the request to the jsp for display
-	 */
-	private void displayView(HttpServletRequest request,
-							 HttpServletResponse response) {
-		/*
-		 * we read out the touchpoint data and add it as an attribute to the
-		 * request such that the jsp can be agnostic with regard to how the data
-		 * to be display has been obtained
-		 */
-		// obtain the executor
-		TouchpointCRUDExecutor exec = (TouchpointCRUDExecutor) getServletContext()
-				.getAttribute("touchpointCRUD");
-		// read out the touchpoints and them to the request
-		request.setAttribute("touchpoints", exec.readAllTouchpoints());
+            // if we have a create request, we read out the parameters
+            String street = request.getParameter("street");
+            String houseNr = request.getParameter("houseNr");
+            String city = request.getParameter("city");
+            String zipCode = request.getParameter("zipCode");
 
-		// obtain a dispatcher for the jsp (which will always be the same view)
-		RequestDispatcher dispatcher = ((HttpServletRequest) request)
-				.getServletContext().getRequestDispatcher("/gui.jsp");
-		// and forward the request to the jsp
-		try {
-			dispatcher.forward(request, response);
-		} catch (Exception e) {
-			String err = "got exception: " + e;
-			logger.error(err, e);
-			throw new RuntimeException(e);
-		}
-	}
+            // create a new address
+            Address addr = new Address(street, houseNr, zipCode, city);
+            StationaryTouchpoint tp = new StationaryTouchpoint(-1, name, addr);
+            exec.createTouchpoint(tp);
+        }
+
+        if (!demoPostNonIdempotence && !demoGetIdempotence) {
+            request.setAttribute("redirectToRoot", true);
+        } else if (demoGetIdempotence) {
+            request.setAttribute("demoGetIdempotence", true);
+        }
+        displayView(request, response);
+    }
+
+    /**
+     * display the view (we always use the same jsp for all requests)
+     */
+    /*
+     * now pass the request to the jsp for display
+     */
+    private void displayView(HttpServletRequest request,
+                             HttpServletResponse response) {
+        /*
+         * we read out the touchpoint data and add it as an attribute to the
+         * request such that the jsp can be agnostic with regard to how the data
+         * to be display has been obtained
+         */
+        // obtain the executor
+        TouchpointCRUDExecutor exec = (TouchpointCRUDExecutor) getServletContext()
+                .getAttribute("touchpointCRUD");
+        // read out the touchpoints and them to the request
+        request.setAttribute("touchpoints", exec.readAllTouchpoints());
+
+        // obtain a dispatcher for the jsp (which will always be the same view)
+        RequestDispatcher dispatcher = ((HttpServletRequest) request)
+                .getServletContext().getRequestDispatcher("/gui.jsp");
+        // and forward the request to the jsp
+        try {
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            String err = "got exception: " + e;
+            logger.error(err, e);
+            throw new RuntimeException(e);
+        }
+    }
 
 }
--- org.dieschnittstelle.ess.ser/src/main/java/org/dieschnittstelle/ess/ser/TouchpointServiceServlet.java
+++ org.dieschnittstelle.ess.ser/src/main/java/org/dieschnittstelle/ess/ser/TouchpointServiceServlet.java
@@ -1,5 +1,7 @@
 package org.dieschnittstelle.ess.ser;
 
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import jakarta.servlet.http.HttpServlet;
@@ -9,84 +11,92 @@ import jakarta.servlet.http.HttpServletResponse;
 import static org.dieschnittstelle.ess.utils.Utils.*;
 
 import org.apache.logging.log4j.Logger;
+import org.dieschnittstelle.ess.entities.crm.AbstractTouchpoint;
 
 public class TouchpointServiceServlet extends HttpServlet {
 
-	protected static Logger logger = org.apache.logging.log4j.LogManager
-			.getLogger(TouchpointServiceServlet.class);
+    protected static Logger logger = org.apache.logging.log4j.LogManager
+            .getLogger(TouchpointServiceServlet.class);
 
-	public TouchpointServiceServlet() {
-		show("TouchpointServiceServlet: constructor invoked\n");
-	}
-	
-	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) {
+    public TouchpointServiceServlet() {
+        show("TouchpointServiceServlet: constructor invoked\n");
+    }
 
-		logger.info("doGet()");
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) {
 
-		// we assume here that GET will only be used to return the list of all
-		// touchpoints
+        logger.info("doGet()?>>>");
 
-		// obtain the executor for reading out the touchpoints
-		TouchpointCRUDExecutor exec = (TouchpointCRUDExecutor) getServletContext()
-				.getAttribute("touchpointCRUD");
-		try {
-			// set the status
-			response.setStatus(HttpServletResponse.SC_OK);
-			// obtain the output stream from the response and write the list of
-			// touchpoints into the stream
-			ObjectOutputStream oos = new ObjectOutputStream(
-					response.getOutputStream());
-			// write the object
-			oos.writeObject(exec.readAllTouchpoints());
-			oos.close();
-		} catch (Exception e) {
-			String err = "got exception: " + e;
-			logger.error(err, e);
-			throw new RuntimeException(e);
-		}
+        // we assume here that GET will only be used to return the list of all
+        // touchpoints
 
-	}
+        // obtain the executor for reading out the touchpoints
+        TouchpointCRUDExecutor exec = (TouchpointCRUDExecutor) getServletContext()
+                .getAttribute("touchpointCRUD");
+        try {
+            // set the status
+            response.setStatus(HttpServletResponse.SC_OK);
+            // obtain the output stream from the response and write the list of
+            // touchpoints into the stream
+            ObjectOutputStream oos = new ObjectOutputStream(
+                    response.getOutputStream());
+            // write the object
+            oos.writeObject(exec.readAllTouchpoints());
+            oos.close();
+        } catch (Exception e) {
+            String err = "got exception: " + e;
+            logger.error(err, e);
+            throw new RuntimeException(e);
+        }
 
-	/*
-	 * TODO: SER3 server-side implementation of createNewTouchpoint
-	 */
-	/*
-	@Override	
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) {
+    }
 
-		// assume POST will only be used for touchpoint creation, i.e. there is
-		// no need to check the uri that has been used
+    /*
+     * TODO: SER3 server-side implementation of createNewTouchpoint
+     */
 
-		// obtain the executor for reading out the touchpoints from the servlet context using the touchpointCRUD attribute
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) {
 
-		try {
-			// create an ObjectInputStream from the request's input stream
-		
-			// read an AbstractTouchpoint object from the stream
-		
-			// call the create method on the executor and take its return value
-		
-			// set the response status as successful, using the appropriate
-			// constant from HttpServletResponse
-		
-			// then write the object to the response's output stream, using a
-			// wrapping ObjectOutputStream
-		
-			// ... and write the object to the stream
-		
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+        // assume POST will only be used for touchpoint creation, i.e. there is
+        // no need to check the uri that has been used
 
-	}
-	*/
+        // obtain the executor for reading out the touchpoints from the servlet context using the touchpointCRUD attribute
+        var executor = (TouchpointCRUDExecutor) getServletContext()
+                .getAttribute("touchpointCRUD");
+        try {
+            // create an ObjectInputStream from the request's input stream
+            var inputStream = new ObjectInputStream(request.getInputStream());
 
-	/*
-	 * TODO: SER4 server-side implementation of deleteTouchpoint
-	 */
+            // read an AbstractTouchpoint object from the stream
+            var newPointRequest = (AbstractTouchpoint) inputStream.readObject();
+
+            // call the create method on the executor and take its return value
+            var newPoint = executor.createTouchpoint(newPointRequest);
+
+            // set the response status as successful, using the appropriate
+            // constant from HttpServletResponse
+            response.setStatus(HttpServletResponse.SC_CREATED);
+
+            // then write the object to the response's output stream, using a
+            // wrapping ObjectOutputStream
+            var outputStream = new ObjectOutputStream(response.getOutputStream());
+
+            // ... and write the object to the stream
+            outputStream.writeObject(newPoint);
+            outputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    /*
+     * TODO: SER4 server-side implementation of deleteTouchpoint
+     */
 
 
 }
